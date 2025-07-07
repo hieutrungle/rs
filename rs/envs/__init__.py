6331823506b80f083bd9d48745eccda1d9a9120d
HieuLe <hieult1996@gmail.com>
2025-07-07 10:21:57 -0500

    add 4UE env

--- rs/envs/__init__.py
+++ rs/envs/__init__.py
@@ -1,4 +1,5 @@
 from rs.envs.conference_2ue_allocation import Conference2UEAllocation
+from rs.envs.conference_4ue_allocation import Conference4UEAllocation
 from rs.envs.classroom_2ue import Classroom2UE
 from rs.envs.classroom_4ue import Classroom4UE
 from rs.envs.classroom import Classroom
