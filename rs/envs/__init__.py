4e341242b6f586d46c2d2fc502b4ae383e55ebf6
HieuLe <hieult1996@gmail.com>
2025-07-16 18:54:37 -0500

    add running multi-agent for 1 reflector with multiple users

--- rs/envs/__init__.py
+++ rs/envs/__init__.py
@@ -5,3 +5,15 @@ from rs.envs.classroom_4ue import Classroom4UE
 from rs.envs.classroom import Classroom
 from rs.envs.classroom_eval import ClassroomEval
 from rs.envs.data_center import TwoAgentDataCenter
+from rs.envs.hallway_1ue_ma import Hallway1UEMA
+
+env_ids = {
+    "conference_2ue_allocation": Conference2UEAllocation,
+    "conference_4ue_allocation": Conference4UEAllocation,
+    "classroom_2ue": Classroom2UE,
+    "classroom_4ue": Classroom4UE,
+    "classroom": Classroom,
+    "classroom_eval": ClassroomEval,
+    "data_center": TwoAgentDataCenter,
+    "hallway_1ue_ma": Hallway1UEMA,
+}
