60dbe3166a68d3b348b476102a9fb7c678e7840d
HieuLe <hieult1996@gmail.com>
2025-08-20 21:45:39 -0500

    outdoor training

--- run_outdoor_2ue.sh
+++ run_outdoor_2ue.sh
@@ -42,7 +42,3 @@ TMP_DIR=${SOURCE_DIR}/tmp
 export OPTIX_CACHE_PATH=${TMP_DIR}/optix_cache
 mkdir -p ${OPTIX_CACHE_PATH}
 python ./main_attention.py --command "train" --env_id "outdoor_2ue" --checkpoint_dir $SOURCE_DIR/test_local_assets/outdoor_2ue --sionna_config_file $SOURCE_DIR/configs/sionna_outdoor_2ue.yaml --source_dir $SOURCE_DIR --num_envs 5 --group "PPO_Allocation_Outdoor_2UE" --name "outdoor_2ue" --track_wandb False
-# --wandb "offline" --no_compatibility_scores True --ep_len 2 --frames_per_batch 8 --n_iters 10 --num_epochs 2 --minibatch_size 4 --wandb "offline" --seed 2
-# --load_model $SOURCE_DIR/test_local_assets/attention_allocator_models_1/checkpoint_2.pt --load_allocator_replay_buffer $SOURCE_DIR/test_local_assets/attention_allocator_models_1/allocator_replay_buffer --load_allocator $SOURCE_DIR/test_local_assets/attention_allocator_models_1/allocator.pt
-# --load_model "/home/hieule/research/rs/local_assets_2/models/checkpoint_1.pt"
-# python ./main.py --command "eval" --checkpoint_dir "/home/hieule/research/rs/local_assets/models" --sionna_config_file "/home/hieule/research/rs/configs/sionna_shared_ap.yaml" --replay_buffer_dir "/home/hieule/research/rs/local_assets/replay_buffer" --wandb "offline" --num_envs 1
